4bf7f676d300c1824478b44877432e7ad9d13ea8
devfunmilayo <marvellousade1011@gmail.com>
2024-07-08 17:33:16 +0100

    alert

--- components/alert/alert.component.tsx
+++ components/alert/alert.component.tsx
@@ -1,7 +1,41 @@
-import { AlertContainer } from "../../styles/alert.styles";
+import React, { useState } from 'react';
+import Image from 'next/image'
+import { AlertContainer } from '../../styles/alert.styles';
 
 const Alert = () => {
-  return <AlertContainer></AlertContainer>;
-};
+    const [isUnsafe, setIsUnsafe] = useState(true);
+    return (
+        <AlertContainer>
+          <div id="textdesign">
+            This Water is 
+          </div>
+          <div>
+            {isUnsafe ? (
+              <>
+                <p>Unsafe for Use</p>
+                <Image 
+                  src="/images/redthumbs.jpg"
+                  width={80}
+                  height={80}
+                  alt=""
+                />
+              </>
+            ) : (
+              <>
+                <p>Safe for Use</p>
+                <Image 
+                  src="/images/greenthumbs.png"
+                  width={80}
+                  height={80}
+                  alt=""
+                />
+              </>
+            )}
+          </div>
+        </AlertContainer>
+      );
+    };
+    
+
 
 export default Alert;
--- styles/alert.styles.tsx
+++ styles/alert.styles.tsx
@@ -1,4 +1,15 @@
 import styled from "styled-components";
-
+import Image from 'next/image';
 export const AlertContainer = styled.section`
-`;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white; 
+  color: black;
+  font-weight: bold;
+  padding: 2rem;
+  font-size: large;
+   left: 70rem;
+  `;
+  
