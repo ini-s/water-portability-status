08a59b2e8b9ccc5960aab7365f98847b5b035ae8
Inioluwa <inisoetan2014@gmail.com>
2024-07-16 21:37:39 +0100

    refactor(data-types.ts): update types

--- types/data-types.ts
+++ types/data-types.ts
@@ -1,9 +1,12 @@
 export interface PropertyData {
   subtitle: string;
   labels: string[];
-  actualValues: number[]; // Optional property
+  values: number[];
   xLabel: string;
   yLabel: string;
+  min: number;
+  max: number;
+  step: number;
 }
 
 export interface IProperty {
@@ -14,4 +17,21 @@ export interface IProperty {
 export interface ICategory {
   category: string;
   properties: IProperty[];
+}
+
+export interface IPredictionLogs {
+  ph: number;
+  portability: boolean;
+  temperature: number;
+}
+export interface IWaterData {
+  created_at: string;
+  electrical_conductivity: number;
+  location: string;
+  ph: number;
+  prediction_log: IPredictionLogs;
+  salinity: number;
+  specific_gravity: number;
+  temperature: number;
+  total_dissolved_solids: number;
 }
