9685c44d4ec2d7a264839cb07c548431c4d5bd09
devfunmilayo <marvellousade1011@gmail.com>
2024-07-18 21:33:18 +0100

    add safety screen component

--- components/safety-screen/safety-screen.component.tsx
+++ components/safety-screen/safety-screen.component.tsx
@@ -1,7 +1,31 @@
-import { SafeScreenContainer } from "../../styles/safety-screen.styles";
+import { ImageContainer, SafeScreenContainer, TextContainer } from "../../styles/safety-screen.styles";
 
-const SafetyScreen = () => {
-  return <SafeScreenContainer></SafeScreenContainer>;
+import Image from "next/image";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const SafetyScreen: React.FC = () => {
+  const [isUnsafe, setIsUnsafe] = useState(true);
+
+  return (
+    <SafeScreenContainer>
+      
+        <TextContainer>
+          <h2>This Water is </h2>
+          <h1>{isUnsafe ? " Unsafe for Use" : " Safe for Use"}</h1>
+        </TextContainer>
+
+        <ImageContainer>
+          <Image
+          src={isUnsafe ? "/images/redthumbs.png" : "/images/greenthumbs.png"}
+          width={200}
+          height={207}
+          alt={isUnsafe ? "" : ""}
+          />
+        </ImageContainer>
+      
+    </SafeScreenContainer>
+  );
 };
 
 export default SafetyScreen;
