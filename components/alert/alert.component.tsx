e4e78665b6ad53ee999a2496f32d7fc5d6249381
devfunmilayo <marvellousade1011@gmail.com>
2024-07-18 21:32:38 +0100

    add alert component

--- components/alert/alert.component.tsx
+++ components/alert/alert.component.tsx
@@ -1,41 +1,49 @@
-import React, { useState } from 'react';
-import Image from 'next/image'
-import { AlertContainer } from '../../styles/alert.styles';
+import React, { useState } from "react";
+
+import { AlertContainer } from "../../styles/alert.styles";
 
 const Alert = () => {
-    const [isUnsafe, setIsUnsafe] = useState(true);
-    return (
-        <AlertContainer>
-          <div id="textdesign">
-            This Water is 
-          </div>
-          <div>
-            {isUnsafe ? (
-              <>
-                <p>Unsafe for Use</p>
-                <Image 
-                  src="/images/redthumbs.jpg"
-                  width={80}
-                  height={80}
-                  alt=""
-                />
-              </>
-            ) : (
-              <>
-                <p>Safe for Use</p>
-                <Image 
-                  src="/images/greenthumbs.png"
-                  width={80}
-                  height={80}
-                  alt=""
-                />
-              </>
-            )}
-          </div>
-        </AlertContainer>
-      );
-    };
-    
-
+  const [isUnsafe, setIsUnsafe] = useState(true)
+  return (
+    <AlertContainer>
+      <p>Report at 21/06/2024 09:00</p>
+      <h1>ALERT</h1>
+      <p>
+        {isUnsafe ? (
+          <>
+            pH is not within acceptable range
+            <br />
+            Temperature is not within acceptable range
+            <br />
+            Specific Gravity is not within acceptable range
+            <br />
+            Total Dissolved Solids is not within acceptable range
+            <br />
+            Salinity is not within acceptable range
+            <br />
+            Electrical Conductivity is not within acceptable range
+          </>
+        ) : (
+          <>
+            pH is within acceptable range
+            <br />
+            Temperature is within acceptable range
+            <br />
+            Specific Gravity is within acceptable range
+            <br />
+            Total Dissolved Solids is within acceptable range
+            <br />
+            Salinity is within acceptable range
+            <br />
+            Electrical Conductivity is within acceptable range
+            
+         
+          </>
+        )}
+      </p>
+      <button>Get Real-time Data</button>
+    </AlertContainer>
+  );
+};
 
 export default Alert;
