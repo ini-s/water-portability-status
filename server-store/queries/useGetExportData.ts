f455b455ca498362aa023eb9d37c0c2b8d448774
Inioluwa <inisoetan2014@gmail.com>
2024-07-24 11:36:12 +0100

    refactor(useGetExportData.ts): enable fetching only if the dates are formatted

--- server-store/queries/useGetExportData.ts
+++ server-store/queries/useGetExportData.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
+
 import { api } from "../utils";
 
 import { IGetAllArgs } from "./useGetNotifications";
 
-const getRealTimeData = async ({
+const getExportData = async ({
   location,
   start_date,
   end_date,
@@ -18,20 +19,25 @@ const getRealTimeData = async ({
   return response.data.data;
 };
 
-const useGetRealTimeData = ({
+const useGetExportData = ({
   location,
   start_date,
   end_date,
 }: Pick<IGetAllArgs, "location" | "start_date" | "end_date">) => {
+  const dateFormat = /^\d{4}-\d{2}-\d{2}$/; // YYYY-MM-DD format
+  const isEnabled =
+    dateFormat.test(start_date ?? "") && dateFormat.test(end_date ?? "");
+
   return useQuery(
     ["EXPORT_DATA", location, start_date, end_date],
-    () => getRealTimeData({ location, start_date, end_date }),
+    () => getExportData({ location, start_date, end_date }),
     {
       keepPreviousData: true,
       retry: false,
-      staleTime: 1000 * 60 * 2 ,
+      staleTime: 1000 * 60 * 2,
+      enabled: isEnabled,
     }
   );
 };
 
-export default useGetRealTimeData;
+export default useGetExportData;
