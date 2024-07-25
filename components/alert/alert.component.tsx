b0db324a64d071f42587bb7c84a45705f64d1cc7
Inioluwa <inisoetan2014@gmail.com>
2024-07-25 16:55:42 +0100

    refactor(alert.component): restructure

--- components/alert/alert.component.tsx
+++ components/alert/alert.component.tsx
@@ -2,12 +2,15 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import dayjs from "dayjs";
 
-import { AlertContainer } from "../../styles/alert.styles";
+import {
+  AlertContainer,
+  AlertHeading,
+  AlertText,
+} from "../../styles/alert.styles";
 
 import useAddLocation from "../../server-store/mutations/useAddLocation";
-
 import { getLocationFromQuery } from "../../server-store/queries/queries";
-import { IGetAllArgs } from "../../server-store/queries/useGetNotifications";
+
 import { IWaterData } from "../../types/data-types";
 
 import ModalComponent from "../modal/modal.component";
@@ -49,38 +52,41 @@ const Alert = ({
         Report at{" "}
         {dayjs(currentWaterData?.created_at).format("MM/DD/YYYY HH:mm")}
       </p>
-      <h1>ALERT</h1>
-      <p style={{ color: isSafe ? "rgba(4, 155, 1, 1)" : "rgb(226, 3, 3)" }}>
-        {!isSafe ? (
-          <>
-            pH is not within acceptable range
-            <br />
-            Temperature is not within acceptable range
-            <br />
-            Specific Gravity is not within acceptable range
-            <br />
-            Total Dissolved Solids is not within acceptable range
-            <br />
-            Salinity is not within acceptable range
-            <br />
-            Electrical Conductivity is not within acceptable range
-          </>
-        ) : (
-          <>
-            pH is within acceptable range
-            <br />
-            Temperature is within acceptable range
-            <br />
-            Specific Gravity is within acceptable range
-            <br />
-            Total Dissolved Solids is within acceptable range
-            <br />
-            Salinity is within acceptable range
-            <br />
-            Electrical Conductivity is within acceptable range
-          </>
-        )}
-      </p>
+      <AlertText>
+        <AlertHeading>ALERT</AlertHeading>
+        <p style={{ color: isSafe ? "rgba(4, 155, 1, 1)" : "rgb(226, 3, 3)" }}>
+          {!isSafe ? (
+            <>
+              pH is not within acceptable range
+              <br />
+              Temperature is not within acceptable range
+              <br />
+              Specific Gravity is not within acceptable range
+              <br />
+              Total Dissolved Solids is not within acceptable range
+              <br />
+              Salinity is not within acceptable range
+              <br />
+              Electrical Conductivity is not within acceptable range
+            </>
+          ) : (
+            <>
+              pH is within acceptable range
+              <br />
+              Temperature is within acceptable range
+              <br />
+              Specific Gravity is within acceptable range
+              <br />
+              Total Dissolved Solids is within acceptable range
+              <br />
+              Salinity is within acceptable range
+              <br />
+              Electrical Conductivity is within acceptable range
+            </>
+          )}
+        </p>
+      </AlertText>
+
       <button disabled={!location} onClick={getRealTimeData}>
         Get Real-time Data
       </button>
