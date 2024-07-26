5ef0311c727330ae2e826a6956f15078253dabee
Inioluwa <inisoetan2014@gmail.com>
2024-07-26 09:24:11 +0100

    refactor(spinner): center the spinner

--- components/spinner-component/spinner-component.tsx
+++ components/spinner-component/spinner-component.tsx
@@ -16,36 +16,40 @@ const SpinnerWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 20vh;
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-  @media(max-width: 37.5em){
+  @media (max-width: 37.5em) {
     height: 15vh;
   }
 
-  &>p{
+  & > p {
     font-weight: 700;
     font-size: 1.25rem;
 
-    @media(max-width: 37.5em){
-      font-size: .9375rem;
+    @media (max-width: 37.5em) {
+      font-size: 0.9375rem;
       font-weight: 600;
     }
   }
 `;
 
 const RotatingSpinner = styled.div`
-  border: .6rem solid rgba(0, 0, 0, 0.1);
+  border: 0.6rem solid rgba(0, 0, 0, 0.1);
   border-left-color: rgb(var(--color-black));
   border-radius: 50%;
   width: 4rem;
-  height:4rem;
+  height: 4rem;
   animation: ${spin} 1s linear infinite;
-  margin-bottom: .625rem;
+  margin-bottom: 0.625rem;
 
-  @media(max-width: 37.5em){
+  @media (max-width: 37.5em) {
     width: 2.3rem;
     height: 2.3rem;
-    border: .35rem solid rgba(0, 0, 0, 0.1);
+    border: 0.35rem solid rgba(0, 0, 0, 0.1);
     border-left-color: rgb(var(--color-black));
   }
 `;
