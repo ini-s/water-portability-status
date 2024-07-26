c23f82e14842d049b29684f3632f1ac26d01c17b
Inioluwa <inisoetan2014@gmail.com>
2024-07-26 09:22:09 +0100

    style(water-quality-data): constrain the width on larger screens

--- styles/water-quality-data.styles.ts
+++ styles/water-quality-data.styles.ts
@@ -1,28 +1,28 @@
 import styled from "styled-components";
 
 export const WaterPortabilityScreen = styled.div`
-  @media screen and (min-width: 60.5625em) {
-    & > div {
-      width: 100%;
-      display: grid;
-      grid-template-columns: 1.6fr 1fr;
-      align-items: start;
-      gap: 4rem;
-      padding: 0 2rem;
-
-      @media screen and (min-width: 75em) {
-        gap: 6rem;
-        padding: 0 4rem;
-      }
-
-      @media screen and (min-width: 90em) {
-        padding: 0 5.5rem;
-      }
-    }
-  }
-
   @media screen and (min-width: 90em) {
     margin: 0 auto;
     max-width: 90rem;
+  }
+`;
+
+export const FlexContainer = styled.div`
+  @media screen and (min-width: 60.5625em) {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1.6fr 1fr;
+    align-items: start;
+    gap: 4rem;
+    padding: 0 2rem;
+  }
+  
+  @media screen and (min-width: 75em) {
+    gap: 6rem;
+    padding: 0 4rem;
+  }
+
+  @media screen and (min-width: 90em) {
+    padding: 0 5.5rem;
   }
 `;
