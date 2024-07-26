34ce88a243dce75fc7b9a919a1c3891801a2a7b0
Inioluwa <inisoetan2014@gmail.com>
2024-07-26 16:38:23 +0100

    refactor(useAddLocation.ts): change type to IWaterData

--- server-store/mutations/useAddLocation.ts
+++ server-store/mutations/useAddLocation.ts
@@ -1,12 +1,13 @@
-import { IGetAllArgs } from "../queries/useGetNotifications";
+import { IWaterData } from "../../types/data-types";
 import { api } from "../utils";
 
 import { useMutation } from "@tanstack/react-query";
 
 type AddLocationArg = string;
 
-const addLocation = async (data: AddLocationArg): Promise<IGetAllArgs> => {
+const addLocation = async (data: AddLocationArg): Promise<IWaterData> => {
   const response = await api.post(`/water-quality-data?location=${data}`, data);
+
   return response.data.data;
 };
 
