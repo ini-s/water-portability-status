34dd2e7767b332a26d4e9ddaa08ebd392ff0ee4d
GINIKAOLUA <oluasandra@gmail.com>
2024-07-31 16:12:07 +0100

    style: add style to graph component

--- styles/graph.styles.ts
+++ styles/graph.styles.ts
@@ -7,14 +7,14 @@ export const GraphContainer = styled.section`
 
   @media (max-width: 25em){
     height: 13rem;
-    padding-left: 0.3rem;
+    padding-left: 0.1rem;
   }
   @media (max-width: 37.5em) and (min-width: 25.0625em) {
     padding-left: 0.1rem ; 
     height: 19rem;
   }
   @media (max-width:62.5em) and (min-width: 37.5625em) {
-    padding-left: 1rem ;
+    padding-left: .6rem ;
     height: 25rem;
   }
-`;
+`; 
